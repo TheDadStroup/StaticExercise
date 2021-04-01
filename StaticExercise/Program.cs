61c551181f53d87fcd189d4785888362f8d84c11
The Dad <Shihen1611@gmail.com>
2021-04-01 10:45:57 -0500

    first draft

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+          var celsius =  TempConverter.FahrenheitToCelsius(68);
+          var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+            Console.WriteLine($"celcius {celsius}");
+
+
+        }
+    }
+}
